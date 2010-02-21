968fede1e89171c22e42392d4bc1fefe77703013
Alexandre Bourget <alex@bourget.cc>
2010-02-20 23:33:08 -0500

    Fixed documentation.

--- webundo/__init__.py
+++ webundo/__init__.py
@@ -1,14 +1,16 @@
 #/usr/bin/env python
 # -=- encoding: utf-8 -=-
 
-"""This module implements Gmail's undo/cancel features, using Pylons or some
-other web frameworks.
+"""This module implements Gmail's `undo/cancel <http://gmailblog.blogspot.com/2009/03/new-in-labs-undo-send.html>`_
+features, using Pylons or some other web frameworks.
 
-Here is a sample Pylons controller (called 'th', who knows why)::
+Here is a sample Pylons controller (called 'th', who knows why):
 
 .. code-block:: python
+
     from pylons import request, response, session, tmpl_context as c
     from YOURPROJECT.lib.base import BaseController, render
+    from routes import url_for
 
     import webundo
 
@@ -24,7 +26,6 @@ Here is a sample Pylons controller (called 'th', who knows why)::
                 open('/tmp/dump.txt', 'w').write(req.params.get('stuff'))
             c.key = webundo.launch_cancelable_job(save, 3)
             return render('saving.mako')
-
         def cancel(self, id):
             if webundo.cancel_job(id):
                 return "Thread cancelled successfully"
@@ -48,6 +49,7 @@ Here is a sample Pylons controller (called 'th', who knows why)::
                 return "Thread was lost already"
             return "Undone: %s" % ret
 
+This shows off a `cancelable job` and an `undoable job`.  Each require
 
 """
 import threading
@@ -75,6 +77,9 @@ def unproxy_closure(func):
     a _current_obj() method.
 
     It could be extended to other frameworks or objects.
+
+    This function is called automatically by ``launch_cancelable_job`` and
+    ``launch_undoable_job`` if the ``unproxy`` parameter is True.
     """
     if not func.func_closure:
         return func
@@ -92,18 +97,28 @@ def unproxy_closure(func):
 
 
 
-def launch_cancelable_job(func, timeout):
+def launch_cancelable_job(func, timeout, unproxy=True):
     """Launch a new cancellable thread, and execute only after the timeout.
 
     This means, if someone calls ``cancel_job``, the function will never be
     executed.
+
+    If you're using Pylons, it's safer to keep ``unproxy`` to True, in case you
+    close-in Pylons' globals variables (request, response, app_globals, etc.).
+    For other frameworks, it won't hurt to leave it on, it's a tiny overhead.
+
+    :param func: timed function to execute after ``timeout``
+    :param timeout: timeout in seconds
+    :param unproxy: whether to clean up the `func` of closed-in proxy objects
+    :return: UUID-key for the launched job, use when calling ``cancel_job``
     """
     store = waiting_threads  # reference for closing in `funcwrap`
     key = str(uuid.uuid4())
     # Uncover proxy objects in closure of `func`.
-    newfunc = unproxy_closure(func)
+    if unproxy:
+        func = unproxy_closure(func)
     def funcwrap():
-        newfunc()
+        func()
         # Delete the reference to the thread in the apps global after running
         if key in store['cancelable_jobs']:
             del store['cancelable_jobs'][key]
@@ -117,6 +132,7 @@ def cancel_job(key):
     """Cancel a job launched with ``launch_cancelable_job``
 
     :param key: uuid returned by a previous call to ``launch_cancelable_job``
+    :return: True if we could cancel the job, False if you were too late
     """
     timer = waiting_threads['cancelable_jobs'].get(key)
     if timer:
@@ -126,14 +142,18 @@ def cancel_job(key):
     return False
 
 
-def launch_undoable_job(func, timeout):
+def launch_undoable_job(func, timeout, unproxy=True):
     """Launch a thread with an undo function.  The function will be called
     only on request through a call to ``undo_job()``, otherwise, it will
     vanish.
 
+    See notes on ``unproxy`` in doc. for ``launch_cancelable_job``.  Same
+    applies here.
+
     :param func: called if undo_job() is called for this job's key.
-    :params timeout: timeout in seconds
-    :rtype The UUID key of this job, used to reference further undo calls.
+    :param timeout: timeout in seconds
+    :param unproxy: whether to clean up the `func` of closed-in proxy objects
+    :return: UUID-key of this job, use when calling ``undo_job``.
     """
     key = str(uuid.uuid4())
     store = waiting_threads  # reference for closing in `run`
@@ -156,8 +176,9 @@ def launch_undoable_job(func, timeout):
             # Accessed from a new request's thread.
             # Block on put
             self.trigger_queue.put('STOP', block=True)
-    newfunc = unproxy_closure(func)
-    obj = Undoable(newfunc, timeout)
+    if unproxy:
+        func = unproxy_closure(func)
+    obj = Undoable(func, timeout)
     thrd = threading.Thread(target=obj.run)
     thrd.start()
     store['undoable_jobs'][key] = obj
@@ -171,6 +192,11 @@ def undo_job(key, timeout=None):
     If the other thread was timed out, the ``undo`` function will not be
     available to trigger anymore, and a ThreadLostError exception will be
     raised.
+
+    :param key: uuid-key returned by ``launch_undoable_job``. Can't be mixed with cancelable jobs.
+    :param timeout: timeout in seconds.
+    :return: value returned by the function passed to ``launch_undoable_job``.
+    :raise ThreadLostError: if the timeout was elapsed, meaning you couldn't undo anymore.
     """
     obj = waiting_threads['undoable_jobs'].get(key)
     if not obj:
